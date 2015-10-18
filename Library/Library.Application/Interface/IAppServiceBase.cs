9e3fde14b8d2626175ba37e484fe429d4b9a0d46
Joel Júnior <junior.tads@gmail.com>
2015-10-18 14:51:22 -0200

    web create

--- Library/Library.Application/Interface/IAppServiceBase.cs
+++ Library/Library.Application/Interface/IAppServiceBase.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Library.Application.Interface
 {
+    [ServiceContract]
     public interface IAppServiceBase<TEntity> where TEntity : class
     {
+        [OperationContract]
         void Add(TEntity obj);
+        [OperationContract]
         TEntity GetById(int id);
+        [OperationContract]
         IEnumerable<TEntity> GetAll();
+        [OperationContract]
         void Update(TEntity obj);
+        [OperationContract]
         void Remove(TEntity obj);
         void Dispose();
     }
