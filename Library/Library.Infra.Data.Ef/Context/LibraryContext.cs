3872b85c74c085e974ad040b5939a4c9c03303b1
Joel Júnior <junior.tads@gmail.com>
2015-10-19 23:40:34 -0200

    bug fix context map

---
+++ Library/Library.Infra.Data.Ef/Context/LibraryContext.cs
@@ -0,0 +1,31 @@
+﻿using Library.Domain.Entities;
+using Library.Infra.Data.Ef.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Infra.Data.Ef.Context
+{
+    public class LibraryContext : DbContext
+    {
+        static LibraryContext()
+        {
+            Database.SetInitializer(new LibrarySeedInitializer());
+        }
+
+        public DbSet<Book> Book { get; set; }
+        public DbSet<DemandsForBook> DemandsForBook { get; set; }
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Configurations.Add(new BookMap());
+            modelBuilder.Configurations.Add(new StudentMap());
+        }
+    }
+}
