415dde50ae3803253ae541beaa4e072f461d9b87
Joel Júnior <junior.tads@gmail.com>
2015-10-19 23:42:41 -0200

    ...

---
+++ Library/Library.Infra.Data.Ef/Mappings/StudentMap.cs
@@ -0,0 +1,18 @@
+﻿using Library.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Infra.Data.Ef.Mappings
+{
+    public class StudentMap : EntityTypeConfiguration<Student>
+    {
+        internal StudentMap()
+        {
+            HasKey(o => o.Id);
+        }
+    }
+}
